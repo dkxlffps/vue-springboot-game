eff51bea20f235f562464450a0677d63958183bb
jihun <kjh1996@interpark.com>
2021-02-05 17:51:04 +0900

    장비생성 하위 컴포넌트 추가

---
+++ src/frontend/src/views/admin/components/equip/Equip.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <common-option :commonOption="setCommonOption" />
+  </div>
+</template>
+<script>
+import commonOption from './Common.vue';
+
+import * as UTIL from 'Script/utils';
+
+export default {
+  name: 'Equip',
+  components: {
+    'common-option': commonOption,
+  },
+  created() {
+    
+  },
+  methods: {
+    setCommonOption(models) {
+      this.equipOption = UTIL.arrayOptionToStr(models);
+    }
+  },
+  data() {
+    return {
+      equipOption: '',
+    }
+  }
+}
+</script>
